a16f547b7eb0de607a826295b4b1f6c12c0fa47d
Interlandi <federico.interlandi@accenture.com>
2024-11-25 15:34:51 -0300

    End of section 3

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NgFor],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit{
+  http = inject(HttpClient)
+  title = 'DatingApp';
+  public users: any;
+  ngOnInit(): void {
+    this.http.get('https://localhost:7088/api/users').subscribe({
+      next: (res) => this.users = res
+    })
+  }
+}
