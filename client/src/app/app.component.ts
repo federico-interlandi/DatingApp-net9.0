d86fe225cf6f0de3ca78492fec453bbedfad3313
Interlandi <federico.interlandi@accenture.com>
2024-11-26 08:02:53 -0300

    client repo copied

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,22 +1,30 @@
-import { NgFor } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./nav/nav.component";
+import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
+import { NgxSpinnerComponent } from 'ngx-spinner';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, NgFor],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, NavComponent, HomeComponent, NgxSpinnerComponent]
 })
-export class AppComponent implements OnInit{
-  http = inject(HttpClient)
-  title = 'DatingApp';
-  public users: any;
+export class AppComponent implements OnInit {
+  private accountService = inject(AccountService);
+
   ngOnInit(): void {
-    this.http.get('https://localhost:7088/api/users').subscribe({
-      next: (res) => this.users = res
-    })
+    this.setCurrentUser();
   }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
+  }
+
+
 }
