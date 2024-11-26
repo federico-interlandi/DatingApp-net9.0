5a18a1ab4b0bb324d1dc649fbb2c2231967d1786
Interlandi <federico.interlandi@accenture.com>
2024-11-26 16:34:32 -0300

    End of section

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,25 +1,28 @@
 using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 [Authorize]
-public class UsersController(DataContext context) : BaseApiController
+public class UsersController(IUserRepository userRepository) : BaseApiController
 {
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>>  GetUsers()
+    public async Task<ActionResult<IEnumerable<MemberDto>>>  GetUsers()
     {
-        var users = await context.Users.ToListAsync();
+        var users = await userRepository.GetMemberAsync();
 
-        return users;
+        return Ok(users);
     }
 
-    [HttpGet("{id:int}")] // /api/users/{id}
-    public async Task<ActionResult<AppUser>> GetUser(int id)
+    [HttpGet("{username}")] 
+    public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
-        var user = await context.Users.FindAsync(id);
+        var user = await userRepository.GetMemberAsync(username);
 
         if(user == null) return NotFound();
 
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,8 @@
 
+using API.Data;
 using API.Extensions;
+using API.Middlewares;
+using Microsoft.EntityFrameworkCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,11 +15,26 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error ocurred during migration");
+}
 
 app.Run();
